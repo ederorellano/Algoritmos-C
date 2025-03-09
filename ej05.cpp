3ab8b0521c02254e6fb2bd73b735f92e2ffde4fb
ederorellano <orellano.cni@gmail.com>
2025-03-09 00:30:05 -0500

    Add ej05.cpp

---
+++ ej05.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+    system("cls");
+    int p;
+    cout<<"PRESUPUESTO ANUAL DE LA FACULTAD"<<endl;
+    cout<<"--------------------------------"<<"\n\n";
+    cout<<"-> 30% para la Escuela de Quimica"<<endl;
+    cout<<"-> 20% para la Escuela de Fisica"<<endl;
+    cout<<"-> 35% para la Escuela de Computacion"<<endl;
+    cout<<"-> 25% para la Escuela de Biologia"<<"\n\n\n";
+    cout<<"\t\tIngrese su presupuesto anual: ";
+    cin>>p;
+    cout<<"\n\n";
+    cout<<"-> Escuela de Quimica: "<<0.3 * p<<endl;
+    cout<<"-> Escuela de Fisica: "<<0.2 * p<<endl;
+    cout<<"-> Escuela de Computacion: "<<0.35 * p<<endl;
+    cout<<"-> Escuela de Biologia: "<<0.25 * p;
+    getch();
+    return 0;
+}
